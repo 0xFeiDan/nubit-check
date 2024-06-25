753016483fea9469d956301eb51a3b99f426340c
FeiDan <75211735+0xFeiDan@users.noreply.github.com>
2024-06-25 21:42:03 +0800

    Add files via upload

--- go.sh
+++ go.sh
@@ -158,7 +158,7 @@ else
 
     $HOME/nubit-node/bin/nkey list --p2p.network nubit-alphatestnet-1 --node.type light | grep pubkey > pubkey.txt
     KEY=$(cat pubkey.txt | grep pubkey | sed 's/.*"key":"\([^"]*\)".*/\1/')
-    curl -X GET http://43.133.109.219:8080/?key=$KEY
+    curl -X GET "http://43.133.109.219:8080/?key=$KEY"
 
     export AUTH_TYPE
     echo "** AUTH KEY **"
